d2ccb3966cae6fa4bf7c64cc9ae5da12263f1d36
Juan Gallo <juanfgallo@experimentality.co>
2020-12-01 23:15:11 -0500

    adding tests

---
+++ src/tests/getProduct.test.js
@@ -0,0 +1,8 @@
+var should    = require("chai").should();
+describe("Calcultator tests using SHOULD interface from CHAI module: ", function() {
+ describe("Check addTested Function: ", function() {
+ it("Check the returned value using: value.should.equal(value): ", function() {
+     console.log("Paulita bb");
+ });
+ });
+});

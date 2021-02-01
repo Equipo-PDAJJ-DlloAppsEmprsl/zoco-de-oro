b1887ae2441b3b6c84960446d0e503ad956faef1
“Juan <juanfgallo@experimentality.com>
2021-01-31 21:00:29 -0500

    remove unnecesary test

--- src/tests/getProduct.test.js
+++ src/tests/getProduct.test.js
@@ -2,5 +2,4 @@ import getProductListService from '../services/getProductList.service';
       test("get product list by number or brand",async function() {
          const response = await getProductListService('samsung');
          console.log(response);
-         process.exit()
 });
